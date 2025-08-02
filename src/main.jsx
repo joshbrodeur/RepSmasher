c4545fc3a4a44ea540590db8ee3b755f69c121e9
joshbrodeur <joshbrodeur@gmail.com>
2025-08-02 14:11:37 -0500

    fix: derive router basename from env

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { StoreProvider } from './store.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/RepSmasher">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <StoreProvider>
         <App />
       </StoreProvider>
