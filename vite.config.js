3127052207344a959f4f26fab69bf2cc2b697a9a
joshbrodeur <joshbrodeur@gmail.com>
2025-08-02 14:37:12 -0500

    Deploy build to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  // Set base for GitHub Pages deployment under repository path
+  base: '/RepSmasher/',
   plugins: [react()],
   server: {
     host: true
